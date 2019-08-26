c1934e7735f2e8a3486d524e9713dd012b093697
jbaptisteq <jb.queralt@gmail.com>
2019-08-26 16:43:35 +0200

    Adding restaurant component, service and model. Adding db.json

---
+++ src/app/restaurant.ts
@@ -0,0 +1,36 @@
+export class Restaurant {
+  id?: number;
+
+  name: string;
+
+  address: string;
+
+  city: string;
+
+  image: string[];
+
+  cookingStyle: string;
+
+  description: string;
+
+  phoneNumber: string;
+
+  website: string;
+
+  note: number;
+
+  constructor(name: string, address: string, city: string,
+              image: string[], cookingStyle: string,
+              description: string, phoneNumber: string,
+              website: string, note: number) {
+    this.name = name;
+    this.address = address;
+    this.city = city;
+    this.image = image;
+    this.cookingStyle = cookingStyle;
+    this.description = description;
+    this.phoneNumber = phoneNumber;
+    this.website = website;
+    this.note = 2.5;
+  }
+}
