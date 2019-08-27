ac22db0e0ec78a0bfef70b763f1635c223b71e3e
jbaptisteq <jb.queralt@gmail.com>
2019-08-27 09:12:38 +0200

    Create Component for Restaurant Form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RestaurantComponent } from './restaurant/restaurant.component';
 import { HttpClientModule } from '@angular/common/http';
+import { FormRestaurantComponent } from './formRestaurant/formRestaurant.component';
 
 @NgModule({
    declarations: [
       AppComponent,
-      RestaurantComponent
+      RestaurantComponent,
+      FormRestaurantComponent
    ],
    imports: [
       BrowserModule,
       AppRoutingModule,
-      HttpClientModule,
+      HttpClientModule
    ],
    providers: [],
    bootstrap: [
---
+++ src/app/formRestaurant/formRestaurant.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-formRestaurant',
+  templateUrl: './formRestaurant.component.html',
+  styleUrls: ['./formRestaurant.component.css']
+})
+export class FormRestaurantComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
